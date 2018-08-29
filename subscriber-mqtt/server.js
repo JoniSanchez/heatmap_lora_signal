d74acc059225f75a5be92283ee8be20893f4b2dc
JoniSanchez <jonisanchezparedes@gmail.com>
2018-08-29 03:18:53 +0200

    parse mqtt message and create docker-compose

--- subscriber-mqtt/server.js
+++ subscriber-mqtt/server.js
@@ -1,5 +1,5 @@
 var mqtt = require('./mqtt.js')
-var client = mqtt.connect('http://localhost');
+var client = mqtt.connect('http://broker');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://mongo:27017/lora";
 
@@ -9,17 +9,32 @@ client.on('connect', function () {
 
 client.on('message', function (topic, message) {
   MongoClient.connect(url, function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
-  var data = { topic: topic.toString(), value: message.toString() };
-  db.collection('documents').insertOne(data, function(err, res) {
-    if (err) throw err;
-    console.log("Document inserted");
-  });
-  db.close();
-  console.log(message.toString());
+	  if(!err) {
+		  console.log("We are connected");
+	  }
+	  var messageStr = message.toString();
+	  var strLeng = messageStr.lenght;
+	  var count = 0;
+	  if (messageStr.indexOf('{"rxpk') + 1 ){
+		  while (messageStr.charAt(count) != '{'){
+	          count = count + 1;
+		  }
+		  var responseStr = messageStr.substring(count, strLeng);	
+		  responseJSON = JSON.parse(responseStr);
+		  var rssi = responseJSON.rxpk[0].rssi;
+		  parseInt(rssi, 10);
+		  rssi = ((rssi + 100) / 2);
+		  var dataPayload = responseJSON.rxpk[0].data;	
+		  var bufferData = Buffer.from(dataPayload, 'base64');
+		  var bufferData = bufferData + " ";
+		  var bufferDataArray = bufferData.split(";");
+		  for(var i = 0; i < bufferDataArray.length - 1; i++) {
+			  bufferDataArray[i] = +bufferDataArray[i];
+		  }
+		  if (bufferDataArray[2] != null){
+			  db.collection('position').insert({latitude:bufferDataArray[0], longitude:bufferDataArray[1], intensity:rssi});
+		  }
+		  db.close();
+	  }
   });
 });
-
-console.log('Client started...');
